e9c7e44fc5d812a242ca49f87a91e6631ba51271
Surajjberaa <berasuraj08@gmail.com>
2025-03-08 03:51:21 +0530

    Added cursor follower

---
+++ src/components/ui/CursorFollower.jsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useRef, useEffect, use, useState } from "react";
+import gsap from "gsap";
+import "./styles.css"; // Import the CSS file
+
+export default function CursorFollower() {
+  const cursorRef = useRef(null);
+  const [isTouchDevice, setIsTouchDevice] = useState(false); // Track touch device status
+
+  useEffect(() => {
+    // Ensure this only runs on the client
+    setIsTouchDevice(typeof window !== "undefined" && "ontouchstart" in window);
+  }, []);
+
+  console.log(isTouchDevice)
+
+  useEffect(() => {
+    if (isTouchDevice || !cursorRef.current) return;
+
+    const cursor = cursorRef.current;
+    const onMouseMove = (e) => {
+      const { target, x, y } = e;
+      const isTargetLinkOrBtn = target?.closest("a") || target?.closest("button") || target?.closest("img");
+
+      gsap.to(cursor, {
+        x: x + 1,
+        y: y + 1,
+        duration: 0.7,
+        ease: "power4",
+        opacity: isTargetLinkOrBtn ? 0.3 : 1,
+        scale: isTargetLinkOrBtn ? 3.5 : 1,
+      });
+    };
+
+    const onMouseLeave = () => {
+      gsap.to(cursor, { duration: 0.7, opacity: 0 });
+    };
+
+    window.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseleave", onMouseLeave);
+
+    return () => {
+      window.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseleave", onMouseLeave);
+    };
+  }, []);
+
+  return <div ref={cursorRef} className="cursor "/>;
+}
